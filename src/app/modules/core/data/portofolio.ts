6e2a1f1f457eeb3f4ec97a9f29f8b98f2a9ca5e2
henki wisnu subakti <henki.subakti@gmail.com>
2020-03-12 15:38:36 +0700

    update portofolio

--- src/app/modules/core/data/portofolio.ts
+++ src/app/modules/core/data/portofolio.ts
@@ -43,8 +43,8 @@ export const Portofolios: Portofolio[] = [{
     resources: '<li>Access the project source on <span class="fancy"><a target="_blank" href="https://github.com/henkiws">GITHUB</a></span></li>'
   },
   {
-    slug: "ecommerce-sepedanesia",
-    title: "E-Commerce \"SEPEDANESIA\"",
+    slug: "sepedanesia",
+    title: "SEPEDANESIA",
     description: "Angular, Lumen",
     images: [{
         url: 'assets/img/portofolio/sepedanesia/sepedanesia1.png',
@@ -54,41 +54,55 @@ export const Portofolios: Portofolio[] = [{
     ],
     about: "<p>Project developed to improve skills and learn new things. I use Angular as a frontend and Lumen as a source API.</p>",
     technical: "<li>Angular</li> <li>Lumen</li> <li>MySql</li> <li>API Raja Ongkir</li> <li>Admin LTE Template</li> <li>API</li>",
+    resources: '<li>Access the project source on <span class="fancy"><a target="_blank" href="https://sepedanesia.com">SEPEDANESIA</a></span></li>'
+  },
+  {
+    slug: "web-blog",
+    title: "Web Blog",
+    description: "Laravel, MySQL",
+    images: [{
+        url: 'https://raw.githubusercontent.com/henkiws/Web-Blog/master/screenshoot/home.png',
+        text: 'Web Blog',
+        desc: ''
+      }
+    ],
+    about: "<p>Project developed to improve skills and learn new things. I use Laravel Framework.</p>",
+    technical: "<li>Laravel</li><li>MySql</li><li>U-Bold Template</li>",
     resources: '<li>Access the project source on <span class="fancy"><a target="_blank" href="https://github.com/henkiws">GITHUB</a></span></li>'
   },
-  {
-    slug: "tour-fyine",
-    title: "Tour Fyine",
-    description: "Angular, Node.js",
-    images: [{
-        url: 'assets/img/portofolio/fyine/fyine1.png',
-        text: 'Tour Fyine',
-        desc: ''
-      },
-      {
-        url: 'assets/img/portofolio/fyine/fyine2.png',
-        text: 'Tour Fyine',
-        desc: ''
-      }
-    ],
-    about: "<p>Office Work and Team Work. We use Angular as a frontend and Node.js as a source API.</p>",
-    technical: "<li>Angular</li> <li>Node Js</li> <li>MongoDB</li> <li>Template Citytours by Ansonika</li>",
-    resources: '<li>The project is online at <span class="fancy"><a target="_blank" href="https://tour.fyine.com">https://tour.fyine.com</a></span></li>'
-  },
-  {
-    slug: "kasir-fyine",
-    title: "Kasir Fyine",
-    description: "Angular, Lumen",
-    images: [{
-        url: 'assets/img/portofolio/kasirfyine/kasirfyine1.png',
-        text: 'Kasir Fyine',
-        desc: ''
-      }
-    ],
-    about: "<p>Office Work and Team Work. We use Angular as a frontend and Lumen as a source API.</p>",
-    technical: "<li>Angular</li> <li>Lumen</li> <li>PostgreSQL</li> <li>Template Admin by Ubold</li>",
-    resources: '<li>The project is online at <span class="fancy"><a target="_blank" href="https://kasir.fyine.com">https://kasir.fyine.com</a></span></li>'
-  },
+  // {
+  //   slug: "tour-fyine",
+  //   title: "Tour Fyine",
+  //   description: "Angular, Node.js",
+  //   images: [{
+  //       url: 'assets/img/portofolio/fyine/fyine1.png',
+  //       text: 'Tour Fyine',
+  //       desc: ''
+  //     },
+  //     {
+  //       url: 'assets/img/portofolio/fyine/fyine2.png',
+  //       text: 'Tour Fyine',
+  //       desc: ''
+  //     }
+  //   ],
+  //   about: "<p>Office Work and Team Work. We use Angular as a frontend and Node.js as a source API.</p>",
+  //   technical: "<li>Angular</li> <li>Node Js</li> <li>MongoDB</li> <li>Template Citytours by Ansonika</li>",
+  //   resources: '<li>The project is online at <span class="fancy"><a target="_blank" href="https://tour.fyine.com">https://tour.fyine.com</a></span></li>'
+  // },
+  // {
+  //   slug: "kasir-fyine",
+  //   title: "Kasir Fyine",
+  //   description: "Angular, Lumen",
+  //   images: [{
+  //       url: 'assets/img/portofolio/kasirfyine/kasirfyine1.png',
+  //       text: 'Kasir Fyine',
+  //       desc: ''
+  //     }
+  //   ],
+  //   about: "<p>Office Work and Team Work. We use Angular as a frontend and Lumen as a source API.</p>",
+  //   technical: "<li>Angular</li> <li>Lumen</li> <li>PostgreSQL</li> <li>Template Admin by Ubold</li>",
+  //   resources: '<li>The project is online at <span class="fancy"><a target="_blank" href="https://kasir.fyine.com">https://kasir.fyine.com</a></span></li>'
+  // },
   {
     slug: "nirwanatranz-travel",
     title: "Nirwanatranz Travel",
